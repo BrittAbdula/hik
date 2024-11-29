3b01b2aad8caca9b2656ebc34ea77bcc5caae8cb
BrittAbdula <aurorozz@gmail.com>
2024-11-29 11:19:25 +0800

    update tags

--- src/data/photos.ts
+++ src/data/photos.ts
@@ -2,201 +2,201 @@ export const photos = [
   {
     id: '1',
     src: '/0c9523e1626ccff82f69b9ae225e6af.jpg',
-    tags: ['入户图'],
+    tags: ['favs', 'cereal'],
     aspectRatio: 16/9,
     featured: true
   },
   {
     id: '2',
     src: '/1-20102Q135420-L.jpg',
-    tags: ['场景图', 'favs'],
+    tags: ['cars', 'farm'],
     aspectRatio: 4/3,
     featured: true
   },
   {
     id: '3',
     src: '/43a8508d20aec6d298d8d1fc1e98ed0.jpg',
-    tags: ['摆场'],
+    tags: ['cicadas', 'door-county'],
     aspectRatio: 3/2
   },
   {
     id: '4',
     src: '/1-220P3222Q4H1.jpg',
-    tags: ['沙发'],
+    tags: ['flower-bee', 'galena'],
     aspectRatio: 1/1
   },
   {
     id: '5',
     src: '/1-20102Q143360-L.jpg',
-    tags: ['书桌'],
+    tags: ['goat', 'favs'],
     aspectRatio: 4/3,
     featured: true
   },
   {
     id: '6',
     src: '/6e52b947bd323bf2cc781b9689bd67e4.mp4',
-    tags: ['书桌'],
+    tags: ['cars', 'cereal'],
     aspectRatio: 4/3,
     featured: true
   },
   {
     id: '7',
     src: '/1-1A953C02-A60.jpg',
-    tags: ['入户图', 'favs'],
+    tags: ['farm', 'cicadas'],
     aspectRatio: 3/2
   },
   {
     id: '8',
     src: '/1-1A953Ba-Y31.jpg',
-    tags: ['场景图'],
+    tags: ['door-county', 'flower-bee'],
     aspectRatio: 1/1
   },
   {
     id: '9',
     src: '/b380610555cc02078edfdd93eced927.jpg',
-    tags: ['摆场', 'favs'],
+    tags: ['galena', 'goat'],
     aspectRatio: 16/9,
     featured: true
   },
   {
     id: '10',
     src: '/10a4fb9b8aa70b0a4058856acd8b7a0.jpg',
-    tags: ['风景'],
+    tags: ['favs', 'cars'],
     aspectRatio: 4/3
   },
   {
     id: '11',
     src: '/2dd0fa75fe0bf28fb9c33a832a5e05b.jpg',
-    tags: ['建筑'],
+    tags: ['farm', 'cereal'],
     aspectRatio: 16/9,
     featured: true
   },
   {
     id: '12',
     src: '/46bc67ba90f1db9df9e0aae1ae522561.mp4',
-    tags: ['视频'],
+    tags: ['cicadas', 'door-county'],
     aspectRatio: 16/9
   },
   {
     id: '13',
     src: '/587a41a9d56971367afbd59ac857ad9.jpg',
-    tags: ['风景'],
+    tags: ['flower-bee', 'galena'],
     aspectRatio: 3/2
   },
   {
     id: '14',
     src: '/5b652ec769c4a5ca7dcb5f1c8b2e0ab.jpg',
-    tags: ['艺术'],
+    tags: ['goat', 'favs'],
     aspectRatio: 4/3
   },
   {
     id: '15',
     src: '/6f24e8dcf618a81007f500f96fff9da.jpg',
-    tags: ['风景'],
+    tags: ['cars', 'farm'],
     aspectRatio: 16/9,
     featured: true
   },
   {
     id: '16',
     src: '/6f7c96589d9e409c7765dfef14eae04.jpg',
-    tags: ['建筑'],
+    tags: ['cereal', 'cicadas'],
     aspectRatio: 4/3
   },
   {
     id: '17',
     src: '/73f49b642dc6b5f5eb9705ecccdb8f8.jpg',
-    tags: ['艺术'],
+    tags: ['door-county', 'flower-bee'],
     aspectRatio: 1/1
   },
   {
     id: '18',
     src: '/898f3bcd9bf5cce8798b64b358a5333.jpg',
-    tags: ['风景'],
+    tags: ['galena', 'goat'],
     aspectRatio: 3/2
   },
   {
     id: '19',
     src: '/9ce8962fd6770e1f8025361e86785c6.jpg',
-    tags: ['艺术'],
+    tags: ['favs', 'cars'],
     aspectRatio: 4/3
   },
   {
     id: '20',
     src: '/VC-13020.jpg',
-    tags: ['建筑'],
+    tags: ['farm', 'cereal'],
     aspectRatio: 16/9
   },
   {
     id: '21',
     src: '/VC-63019.jpg',
-    tags: ['风景'],
+    tags: ['cicadas', 'door-county'],
     aspectRatio: 3/2
   },
   {
     id: '22',
     src: '/VT-63036-2.jpg',
-    tags: ['艺术'],
+    tags: ['flower-bee', 'galena'],
     aspectRatio: 4/3
   },
   {
     id: '23',
     src: '/VY-4303-1.jpg',
-    tags: ['风景'],
+    tags: ['goat', 'favs'],
     aspectRatio: 1/1
   },
   {
     id: '24',
     src: '/VY-4303.jpg',
-    tags: ['建筑'],
+    tags: ['cars', 'farm'],
     aspectRatio: 3/2
   },
   {
     id: '25',
     src: '/VY-5303-1.jpg',
-    tags: ['艺术'],
+    tags: ['cereal', 'cicadas'],
     aspectRatio: 4/3
   },
   {
     id: '26',
     src: '/VY-5303-2.jpg',
-    tags: ['风景'],
+    tags: ['door-county', 'flower-bee'],
     aspectRatio: 16/9,
     featured: true
   },
   {
     id: '27',
     src: '/VY-5303.jpg',
-    tags: ['建筑'],
+    tags: ['galena', 'goat'],
     aspectRatio: 4/3
   },
   {
     id: '28',
     src: '/VY-5305-1.jpg',
-    tags: ['艺术'],
+    tags: ['favs', 'cars'],
     aspectRatio: 1/1
   },
   {
     id: '29',
     src: '/VY-5305.jpg',
-    tags: ['风景'],
+    tags: ['farm', 'cereal'],
     aspectRatio: 3/2
   },
   {
     id: '30',
     src: '/b5756aec3b7cc841dbd8f29fc3c6148.jpg',
-    tags: ['建筑'],
+    tags: ['cicadas', 'door-county'],
     aspectRatio: 4/3
   },
   {
     id: '31',
     src: '/温岭都市壹号（5月）.mp4',
-    tags: ['视频'],
+    tags: ['flower-bee', 'galena'],
     aspectRatio: 16/9
   },
   {
     id: '32',
     src: '/广西南宁店国宾一号225平.mp4',
-    tags: ['视频'],
+    tags: ['goat', 'favs'],
     aspectRatio: 16/9
   }
 ];
